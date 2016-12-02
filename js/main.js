802ebabb383b560feda50ebf9726cf18a9dfcf17
David St.Amour <davids@rockfordpack.com>
2016-12-02 09:26:25 -0500

    working checkbox parent/child

--- js/main.js
+++ js/main.js
@@ -2,19 +2,20 @@ Vue.component('currency-input', {
     // <input v-bind:value="value" v-on:input="updateValue($event.target.value)" >
     template: '<div>' +
         '<input type="checkbox" v-bind:value="test" v-on:change="updateValue" v-bind:checked="isChecked">' +
+        '<input type="checkbox" value="test111" v-on:change="updateValue2" v-bind:checked="isChecked">' +
         '<p>child: {{checked}}</p><p>test: {{test}}</p></div>',
     props: ['checked', 'test'],
     computed: {
         isChecked: function() {
             return this.checked;
-        },
-        isTest: function() {
-            return this.test;
         }
     },
     methods: {
         updateValue: function(value) {
             this.$emit('input', value);
+        },
+        updateValue2: function(value) {
+            this.$emit('input2', value);
         }
     }
 });
@@ -30,6 +31,10 @@ app = new Vue({
         select: function(e) {
             // alert(e.target.checked);
             this.pchecked = e.target.checked;
+        },
+        select2: function(e) {
+            alert(e.target.checked + 'other box');
+            // this.pchecked = e.target.checked;
         }
     }
 });
